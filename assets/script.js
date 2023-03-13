51bb491b7b0fa0d887b6792aadb388e0bbc95fb0
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-03-13 20:35:53 -0300

    save

--- assets/script.js
+++ assets/script.js
@@ -3,10 +3,36 @@ console.log(campos)
 
 const spans = document.querySelectorAll('.span-required')
 
+const emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+
+function aplicaErro(index) {
+    campos[index].style.border = '2px solid #e63636'
+    spans[index].style.display = 'block'
+}
+
+function removeErro (index) {
+    campos[index].style.border = ''
+    spans[index].style.display = 'none'
+}
+
+
+// Se a quantidade de caracteres em (nome) for menor do que 3 o código aciona a função que habilita o span do HTML, pinta e pinta a borda de vermelho. ('O span é a mensagem de erro definida no HTML'). Do contrário, o erro é removido, bem com as suas características.
+
 function validanome () {
     if (campos[0].value.length < 3 ) {
-        console.log('lararu')
+        aplicaErro(0)
     } else {
-        console.log('Ohhh brabo')
+        removeErro (0)
+    }
+
+}
+
+
+function validaemail () {
+    if(!emailRegex.test(campos[1].value)){
+        aplicaErro(1)
+    } else {
+        removeErro(1)
     }
 }
