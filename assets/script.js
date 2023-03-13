752b94f512f7fd896185b2157af49b96e2419ab8
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-03-13 20:54:49 -0300

    save

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,44 @@
 const campos = document.querySelectorAll("[required]")
-console.log(campos)
 
 const spans = document.querySelectorAll('.span-required')
 
 const emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
+const form = document.querySelector('.form')
+
+
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validanome ();
+    validaemail ();
+    console.log('erro')
+});
+
+
+
+
+
+//Aplica a estilização caso o critério não seja atendido
 
 function aplicaErro(index) {
     campos[index].style.border = '2px solid #e63636'
     spans[index].style.display = 'block'
 }
 
+
+
+//remove a estilização caso o critério seja atendido
+
 function removeErro (index) {
     campos[index].style.border = ''
     spans[index].style.display = 'none'
 }
+
+
+
+
 
 
 // Se a quantidade de caracteres em (nome) for menor do que 3 o código aciona a função que habilita o span do HTML, pinta e pinta a borda de vermelho. ('O span é a mensagem de erro definida no HTML'). Do contrário, o erro é removido, bem com as suas características.
@@ -28,6 +52,8 @@ function validanome () {
 
 }
 
+
+//Aplica uma validação com base no REGEX criado para o email e retorna um erro caso o dado não esteja em conformidade
 
 function validaemail () {
     if(!emailRegex.test(campos[1].value)){
