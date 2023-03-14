3f04060c69d290e94a235c42bd0d962590d5d2b9
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-03-14 15:31:48 -0300

    save

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,20 @@ const emailRegex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const form = document.querySelector('.form')
 
 
+let arraymult = [10,9,8,7,6,5,4,3,2]
+let arraycpf = []
+let arraysoma = []
+
+let contador = 0
 
 
+//Ao clicar no botão enviar, todas as validações serão feitas ao mesmo tempo, através de um ouvinte de eventos no (form)
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     validanome ();
     validaemail ();
     console.log('erro')
 });
-
-
 
 
 
@@ -35,8 +39,6 @@ function removeErro (index) {
     campos[index].style.border = ''
     spans[index].style.display = 'none'
 }
-
-
 
 
 
@@ -61,4 +63,65 @@ function validaemail () {
     } else {
         removeErro(1)
     }
+}
+
+
+function validaCPF () {
+
+
+
+    //Pega os valores digitados no campo CPF e remove os carcteres especiais (\, ., -), após esse tratamento, o dado é armazenado na variável CPF
+    const cpf = campos[3].value.replace(/\.|-/g, "")
+
+    
+
+    console.log (cpf)
+
+
+    //Insere os primeiros 9 digitos dentro do array
+    arraycpf = cpf.slice(0, 9).split("")
+
+    console.log (arraycpf)
+    
+
+    console.log(arraymult)
+
+
+    
+        
+        // O (arraysoma) recebe o resultado da multiplicação entre o (arraycpf) e o (arraymult)
+        arraysoma[contador] = arraycpf[contador] * arraymult[contador]
+
+        
+        contador = contador + 1
+    
+
+        
+        //Se o arraysoma for maior que 11, remova as duas casas, pois precisarei apenas dos primeiros 9 caracteres
+        if ( arraysoma.length == 11) {
+            arraysoma.splice(-2, 2)
+            console.log(arraysoma)
+
+
+            //For para somar toos os valores de (arraysoma)
+
+            let soma = 0;
+            for (let i = 0; i < arraysoma.length; i++) {
+              soma += arraysoma[i];
+            }
+
+
+            //A variável soma recebe o resto da divisão de (arraysoma) por 11, subtraído por 8
+            soma = (soma % 11) - 8
+            
+           
+
+            /*
+            11-8 <= 9 TRUE
+
+            != 11-8 <= 9 FALSE
+            */
+       
+        }
+
 }
